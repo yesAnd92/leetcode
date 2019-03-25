a14296998af5a8bca4075adf82752debf40fffa5
wangyj <850208971@qq.com>
2019-03-25 17:32:38 +0800

    init project

---
+++ src/problems/Problem198.java
@@ -0,0 +1,25 @@
+package problems;
+
+/* *
+ * https://leetcode.com/problems/house-robber/
+ *Given a list of non-negative integers representing the amount of money of each house,
+ *determine the maximum amount of money you can rob tonight without alerting the police.
+ *  Example 2:
+ *  Input: [2,7,9,3,1]
+ *  Output: 12
+ *  Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+ *  Total amount you can rob = 2 + 9 + 1 = 12.
+ *
+ * @author wangyj
+ * @date 2019/3/25 17:06
+ */
+public class Problem198 {
+
+    public static void main(String[] args) {
+
+    }
+
+//    public int rob(int[] nums) {
+//
+//    }
+}
