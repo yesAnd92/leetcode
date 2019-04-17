9d5f41cc539a8198ef2390dfebb23da6dcac0d55
wangyj <850208971@qq.com>
2019-04-17 18:29:29 +0800

    slove problem 139

---
+++ src/problems/WordBreak139.java
@@ -0,0 +1,58 @@
+package problems;
+
+import java.util.*;
+
+public class WordBreak139 {
+    public static void main(String[] args) {
+        List<String> wordDict = Arrays.asList("apple", "pen");
+        System.out.println(wordBreak2("applepenapple", wordDict));
+    }
+
+
+    public static boolean wordBreak2(String s, List<String> wordDict) {
+
+        int length =s.length();
+        //设置opt[i]为从0到i中s.subString(0,i)是否有子串可以组成
+        boolean[] opt=new boolean[length];
+        opt[0]=true;
+        for(int i=1;i<=length;i++){
+            for (int j=0;j<i;j++){
+                boolean contain =wordDict.contains(s.substring(j,i));
+                if (opt[j]&&contain){
+                    opt[i]=true;
+                    break;
+                }
+            }
+        }
+        return opt[length];
+    }
+
+    /**
+     * 使用wordDict的子串进行组合后与目标字符串进行对比
+     * 这种方法解决不了一个子串频繁被使用的问题
+     */
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Map<Integer, List<String>> optList = new HashMap<>();
+        optList.put(0, Arrays.asList(wordDict.get(0)));
+        int length = wordDict.size();
+        for (int i = 1; i < length; i++) {
+            List<String> opt = new ArrayList<>();
+            List<String> beforeOpt = optList.get(i - 1);
+            String word = wordDict.get(i);
+            for (String ele : beforeOpt) {
+                opt.add(ele);
+                opt.add(ele + word);
+                opt.add(word + ele);
+            }
+            opt.add(word);
+            optList.put(i, opt);
+        }
+        boolean result = false;
+        for (String str : optList.get(length - 1)) {
+            if (str.equals(s)) {
+                result = true;
+            }
+        }
+        return result;
+    }
+}
