27dd16ea34c09b7e78dfefed4dd0481aa53e92a9
wangyingjie <wangyingjie@pdmi.cn>
2021-05-07 17:25:35 +0800

    整理目录

---
+++ src/problems/hash_table/TwoSum_1.java
@@ -0,0 +1,24 @@
+package problems.hash_table;
+
+
+/**
+ * https://leetcode.com/problems/two-sum/
+ * @author W.Y.J
+ * @Date 2021/5/7 17:17
+ */
+
+public class TwoSum_1 {
+
+
+    /**
+     * 暴力 O(n^2)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] result =new int[2];
+
+        return result;
+    }
+}
