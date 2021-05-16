e2db70a13d5958a18a46419a83a1762dc7f129f9
wangyingjie <wangyingjie@pdmi.cn>
2021-05-16 18:59:04 +0800

    complete

--- src/main/java/com.wangyj.problems/linked_list/ReverseNodesInKGroup_25.java
+++ src/main/java/com.wangyj.problems/linked_list/ReverseNodesInKGroup_25.java
@@ -53,30 +53,55 @@ public class ReverseNodesInKGroup_25 {
 
 
     /**
-     * 利用栈结构进行反转
+     * 不适用栈，直接反转链表
+     * https://leetcode-cn.com/problems/reverse-nodes-in-k-group/solution/tu-jie-kge-yi-zu-fan-zhuan-lian-biao-by-user7208t/
      */
     public ListNode reverseKGroup2(ListNode head, int k) {
         ListNode dummy = new ListNode();
-        ListNode pre = head;
-        ListNode end = head;//记录每次累计到k，需要反转的位置
-        while (end != null) {
+        dummy.next=head;
+        ListNode pre = dummy;
+        ListNode end = dummy;//记录每次累计到k，需要反转的位置
+        while (end.next != null) {
             //当k个节点时，需要反转
             for (int i = 0; i < k && end != null; i++) {
                 end = end.next;
             }
-
-
+            if (end == null) break;
+            //记录下次开始的位置
+            ListNode next = end.next;
+            //需要反转开始的位置
+            ListNode start = pre.next;
+            //切断后续联系
+            end.next = null;
+            //反转
+            pre.next = reverse(start);
+            start.next=next;
+            pre=start;
+            end=pre;
         }
 
         return dummy.next;
     }
 
 
+    private ListNode reverse(ListNode start) {
+
+        ListNode pre = null;
+        ListNode curr=start;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
     @Test
     public void test() {
         List<Integer> list = new ArrayList<>();
         ListNode n1 = ListNodeUtil.convert2ListNode(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        ListNode result = reverseKGroup(n1, 4);
+        ListNode result = reverseKGroup2(n1, 4);
 
         ListNodeUtil.print(result);
 
