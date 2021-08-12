f6f6a310a4daab50fb681cee77d5ceb6a0bb9ba6
wangyingjie <wangyingjie@pdmi.cn>
2021-08-12 11:49:26 +0800

    add p300

---
+++ src/main/java/com/wangyj/problems/dynamic_programming/LongestIncreasingSubseq_300.java
@@ -0,0 +1,54 @@
+package com.wangyj.problems.dynamic_programming;
+
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 最长递增子序列
+ * https://leetcode-cn.com/problems/longest-increasing-subsequence/
+ *
+ * @author W.Y.J
+ * @Date 2021/8/12 9:19
+ */
+public class LongestIncreasingSubseq_300 {
+
+    /**
+     * 可以参考这篇文章 https://leetcode-cn.com/problems/longest-increasing-subsequence/solution/dong-tai-gui-hua-er-fen-cha-zhao-tan-xin-suan-fa-p/
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+
+        int ans = 1;
+        int length = nums.length;
+
+        //dp[i]表示以第i个元素为结尾的最长递增子序列长度,结尾一定是要包含第i个元素的
+        int[] dp = new int[length];
+        //对dp进行初始化，每个字符作为结尾长度至少都是1
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < length; i++) {
+            //如果第i个元素是最长递增子序列的最后一个值，那么i元素前边所有比第i个元素小的都有可能是
+            //最长递增子序列中的倒数第二个元素，需要进行比较确定最长
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+
+            if (ans < dp[i]) {
+                ans = dp[i];
+            }
+        }
+        return ans;
+    }
+
+
+    @Test
+    public void test() {
+        int[] nums = new int[]{0,1,0,3,2,3};
+        System.out.println(lengthOfLIS(nums));
+    }
+}
