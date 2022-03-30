67a0926ca9dfe79989c00af62b2b2a18358259a7
wangyj <850208971@qq.com>
2022-03-30 18:53:01 +0800

    增加解法

--- src/main/java/com/wangyj/problems/heap/MinisterKNumber_offer40.java
+++ src/main/java/com/wangyj/problems/heap/MinisterKNumber_offer40.java
@@ -10,6 +10,7 @@ import java.util.Queue;
 /**
  * 最小的k个数
  * https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
+ *
  * @author W.Y.J
  * @Date 2022/3/25 10:36
  */
@@ -21,14 +22,14 @@ public class MinisterKNumber_offer40 {
      */
     public int[] getLeastNumbers(int[] arr, int k) {
 
-        if (k==0)return new int[]{};
+        if (k == 0) return new int[]{};
 
         //创建一个大顶堆，大的元素在上边
-        Queue<Integer> heap= new PriorityQueue<>(k, (n1, n2) -> n2 - n1);
+        Queue<Integer> heap = new PriorityQueue<>(k, (n1, n2) -> n2 - n1);
 
-        for (int i = 0; i <arr.length ; i++) {
+        for (int i = 0; i < arr.length; i++) {
 
-            if(heap.isEmpty()||heap.size()<k||arr[i]<heap.peek()){
+            if (heap.isEmpty() || heap.size() < k || arr[i] < heap.peek()) {
                 /**
                  * 这里有个优化，不是所有新遍历的元素都往heap里方，如果heap总已经有k个元素了，
                  * 这时遍历到的当前元素如果比堆顶元素还要大，那么它必然不属于答案的集合
@@ -36,18 +37,67 @@ public class MinisterKNumber_offer40 {
                 heap.offer(arr[i]);
             }
             //如果heap元素超过k个，说明堆顶元素可以被淘汰了
-            if (heap.size()>k){
+            if (heap.size() > k) {
                 heap.poll();
             }
         }
-        return heap.stream().mapToInt(o-> o.intValue()).toArray();
+        return heap.stream().mapToInt(o -> o.intValue()).toArray();
     }
 
 
-    @Test
-    public void test(){
+    public int[] getLeastNumbers2(int[] arr, int k) {
 
-        int[] leastNumbers = getLeastNumbers(new int[]{1, 2,8,2, 3, 4, 5, 6, 7}, 0);
+        if (k == 0) return new int[]{};
+        quicksort(arr,k,0,arr.length-1);
+
+        System.out.println(arr);
+        return null;
+    }
+
+
+    private void quicksort(int[] arr, int k, int left, int right) {
+
+        if (left>right)
+            return;
+        int pivotIndex = partition(arr, left, right);
+        if (pivotIndex==k){
+            return;
+        }else if (k<pivotIndex){
+            //最小的k个数一定在[left,pivotIndex-1]中
+            quicksort(arr,k,left,pivotIndex-1);
+        }else{
+            //最小的k个数一定在[left,pivotIndex]和[pivotIndex+1，k]中
+            quicksort(arr,k,pivotIndex+1,right);
+        }
+    }
+
+    private int partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        while (left < right) {
+            while (arr[left]<pivot&&left<right){
+                left++;
+            }
+            while (arr[right]>pivot&&left<right){
+                right--;
+            }
+            swap(arr,left,right);
+        }
+        int pivotIndex=left++;
+        arr[pivotIndex]=pivot;
+        return pivotIndex;
+    }
+
+
+    private void swap(int[] arr,int left,int right){
+        int tmp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=tmp;
+    }
+
+    @Test
+    public void test() {
+
+        int[] leastNumbers = getLeastNumbers2(new int[]{1, 2, 8, 2, 3, 4, 5, 6, 7}, 4);
         Arrays.stream(leastNumbers).forEach(System.out::print);
     }
 }
